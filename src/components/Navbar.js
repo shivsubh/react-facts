ca84408364396aef132b7431708c001cd0feb21a
Shubham <shubhamshivaay67@gmail.com>
2022-01-21 18:52:53 +0530

    Added alt text in img tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import reactLogo from "../images/react-icon-small.png";
 export default function Navbar(){
     return (
         <nav>
-            <img src= {reactLogo} className="nav--icon"/>
+            <img src={reactLogo} className="nav--icon" alt="logo of react"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
