0608c13357d669b9d2efc62c47fade05adfa11cd
Shubham <shubhamshivaay67@gmail.com>
2022-01-31 13:48:57 +0530

    react facts reformed having dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import React from "react"
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 export default function App() {
+  const [darkMode, setDarkMode] = React.useState(true)
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode)
+  }
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode}
+      />
+      <Main darkMode={darkMode}/>
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React from "react"
-export default function Main(){
+export default function Main(props){
     return (
-        <main>
-            <h1 className="main--title">Fun Facts About React</h1>
+        <main className={props.darkMode ? "dark" : ""}>
+            <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
@@ -10,6 +10,6 @@ export default function Main(){
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </main>   
+        </main> 
     )
 }
